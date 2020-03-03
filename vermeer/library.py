0156c4ba8dcb1e77eb3d188af97cf0282435be71
Krateng <git.noreply@krateng.ch>
2020-03-03 17:10:38 +0100

    Refactored into classes

--- vermeer/library.py
+++ vermeer/library.py
@@ -3,10 +3,119 @@ from wand.image import Image as WandImage
 from io import BytesIO
 
 
-CIRCLEMASK = Image.new("L", (152,152), 0)
-draw = ImageDraw.Draw(CIRCLEMASK)
-draw.ellipse((0, 0, 152, 152), fill=255)
-EMPTY = Image.new("RGBA", (152,152), 0)
+
+
+class AbstractPdxImage:
+	def __init__(self,*args):
+		# create empty image of correct dimensions
+		if len(args) == 0:
+			self.srcimage = self.empty()
+			self.resultimage = self.srcimage
+		
+		# create from file
+		else:
+			f = args[0]
+			self.srcimage = load_from_file(f)
+			
+			# SCALE
+			width, height = self.srcimage.width, self.srcimage.height
+			targetwidth, targetheight = self.DIMENSIONS
+			scale = max(targetwidth/width,targetheight/height)
+			if scale != 1:
+				self.resultimage = self.srcimage.resize((int(scale*width),int(scale*height)))
+			else:
+				self.resultimage = self.srcimage
+				
+			# CROP TO RATIO
+			width, height = self.resultimage.width, self.resultimage.height
+			left_offset = (width - targetwidth) / 2
+			top_offset = (height - targetheight) / 2
+			self.resultimage = self.resultimage.crop((left_offset,top_offset,targetwidth+left_offset,targetheight+top_offset))
+			
+			# CROP SHAPE
+			self.resultimage = self.crop(self.resultimage)
+		
+	
+	def crop(self,img):
+		return img
+		
+		
+	def write(self,f,also_write_png=True):
+		write_to_dds_file(self.resultimage,f)
+		if also_write_png:
+			write_to_png_file(self.resultimage,".".join(f.split(".")[:-1] + ["png"]))
+	
+	# get empty frame	
+	def empty(self):
+		return Image.new("RGBA",self.DIMENSIONS)
+		
+	def show(self):
+		self.resultimage.show()
+
+
+class Sprite(AbstractPdxImage):
+	def __init__(self,*args,imagetype=None,**kwargs):
+		# create from images
+		if all(isinstance(a,AbstractPdxImage) for a in args):
+			self.create(*args,**kwargs)
+			
+		# load existing
+		elif len(args) == 1:
+			self.resultimage = load_from_file(args[0])
+			self.DIMENSIONS = self.resultimage.width, self.resultimage.height
+			# assume that its frames are portraits as we cannot know what it was created from
+			self.imagetype = imagetype or Portrait
+			
+	def create(self,*imgs,lead_empty=0):
+		self.imagetype = type(imgs[0])
+		assert all(isinstance(i,self.imagetype) for i in imgs)
+		frames = [self.imagetype()] * lead_empty + list(imgs)
+		
+		maxnum = self.imagetype.SPRITE_SIZE
+		num = len(frames)
+		assert num <= maxnum
+		
+		
+		height = self.imagetype.DIMENSIONS[1]
+		width = self.imagetype.DIMENSIONS[0] * num
+		self.DIMENSIONS = (width,height)
+
+		
+
+		self.resultimage = Image.new("RGBA",(width,height))
+		j = 0
+		for f in frames:
+			self.insert(f,j)
+			j += 1
+
+		
+	def insert(self,img,index):
+		self.resultimage.paste(img.resultimage,(index*self.imagetype.DIMENSIONS[0],0))
+		
+		
+		
+class Portrait(AbstractPdxImage):
+	DIMENSIONS = (152,152)
+	SPRITE_SIZE = 26
+	
+	def crop(self,img):
+		mask = Image.new("L", (152,152), 0)
+		draw = ImageDraw.Draw(mask)
+		draw.ellipse((0, 0, 152, 152), fill=255)
+		return Image.composite(img,self.empty(),mask)
+	
+class EventPicture(AbstractPdxImage):
+	DIMENSIONS = (450,150)
+	SPRITE_SIZE = 1
+	
+class TraitIcon(AbstractPdxImage):
+	DIMENSIONS = (24,24)
+	SPRITE_SIZE = 1
+
+
+
+
+
 
 
 def load_from_file(f):
@@ -27,41 +136,8 @@ def load_from_dds_file(f):
 	tmp.seek(0)
 	img = Image.open(tmp)
 	return img
-
-def crop(img):
-	# SCALE
-	width, height = img.width, img.height
-	scale = 152 / min(width,height)
-	if scale != 1:
-		img = img.resize((int(scale*width),int(scale*height)))
-	# CROP TO SQUARE
-	width, height = img.width, img.height
-	left_offset = (width - 152) / 2
-	top_offset = (height - 152) / 2
-	img = img.crop((left_offset,top_offset,152+left_offset,152+top_offset))
-	# CROP TO CIRCLE
-	img = Image.composite(img,EMPTY,CIRCLEMASK)
 	
-	return img
 	
-def combine(*imgs,lead_empty=0):
-
-	imgs = [EMPTY] * lead_empty + list(imgs)
-
-	num = len(imgs)
-
-	img = Image.new("RGBA",(152*(len(imgs)),152))
-	j = 0
-	for i in imgs:
-		insert(i,img,j)
-		j += 1
-		
-	return img
-	
-def insert(src,target,index):
-	target.paste(src,(index*152,0))
-	
-
 def write_to_dds_file(img,f):
 			
 	tmp = BytesIO()
@@ -73,5 +149,8 @@ def write_to_dds_file(img,f):
 	
 def write_to_png_file(img,f):
 	img.save(f)
+	
+	
+
 	
 
